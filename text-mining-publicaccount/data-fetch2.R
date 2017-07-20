d1943fd656e971a7f25e54efef5a99a48078b29c
WangLiuying <13656036646@163.com>
2017-07-20 21:44:21 +0800

    nothing new

--- text-mining-publicaccount/data-fetch2.R
+++ text-mining-publicaccount/data-fetch2.R
@@ -2,16 +2,26 @@
 library(rvest)
 library(stringr)
 
-account.list <-
-  c("realmovie520","shenyebagua818", "gossipmaker", "yansubagua")
 df <- data.frame()
+
+##处理超时问题，休息10分钟后重试
+readingURL <- function(link)
+{
+  link_page <- tryCatch(read_html(link),
+                        error=function(e){
+                          print(e);
+                          if(str_detect(e,pattern="405")) 
+                          {Sys.sleep(300);readingURL(link)}
+                          else {return(read_html("<html><title>NA<title></html>"))}
+                        })
+  return(link_page)
+}
 
 #################################################################
 #设置:统一从2月中旬开始吧
 account <- "dsmovie"
-  page <- seq(228,800,by=12)
-  #record：严肃八卦180,192,204,216,228,240,252,264,276,288,300,312,324,336,348,360-444;-780
-  #realmovie:180,192,204,216,240,252,264,276,288,300,312,324,336,348,360,372,384
+  page <- seq(264,800,by=12)
+
   (url <-
     paste("http://chuansong.me/account/",
           account,
@@ -21,7 +31,8 @@ account <- "dsmovie"
 ##################################################################  
   for (i in seq_along(url))
   {
-    url_page <- read_html(url[i])
+    #url_page <- read_html(url[i])
+    url_page <- readingURL(url[i])
     links <- url_page %>% html_nodes(".question_link") %>%
       html_attr("href")
     cat("page",url[i],"\n")
@@ -29,7 +40,8 @@ account <- "dsmovie"
     for (link in links)
     {
       link <- paste("http://chuansong.me", link, sep = "")
-      link_page <-read_html(link)
+      link_page <- readingURL(link)
+      #link_page <-read_html(link)
       body <- link_page %>% html_nodes("#js_content>*") %>% html_text()
       body <- body[nchar(body) > 3]
       body <- Reduce(f = paste, body)
@@ -43,13 +55,14 @@ account <- "dsmovie"
       like <- as.numeric(hot[2])
       original <-
         link_page %>% html_node("#copyright_logo") %>% html_text()
-      if(is.null(body)) body=NA
-      tryCatch(df <- rbind(df, data.frame(title, time, read, like, original,body)),
+      if(!is.na(title))
+        {
+        tryCatch(df <- rbind(df, data.frame(title, time, read, like, original,body)),
                      error=function(e){cat("fail in fetching this document.\n")})
-      cat("获取：", title, "\n")
-      Sys.sleep(runif(1,5,20))
-      Sys.sleep(sample(x = c(0,0,0,1),size = 1)*20)
-      
+        cat("获取：", title, "\n")
+        Sys.sleep(runif(1,5,20))
+        Sys.sleep(sample(x = c(0,0,0,1),size = 1)*20)
+        }  
     }
     if(sample(c(0,0,0,1),1)) {cat("sleep for 10 min\n");Sys.sleep(600)}
     save.image()
@@ -58,3 +71,6 @@ save.image()
 ######################################################################  
 save(df, file = paste(account, ".RData"))
 View(df)
+
+
+
