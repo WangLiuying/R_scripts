d596f5562320de0a54e54448c738568ed440ab1e
Wang,Liuying <13656036646@163.com>
2017-11-13 23:16:06 +0800

    fix function
    重新包装了一下函数

--- lesson_glm/ridgeRegression_udf.R
+++ lesson_glm/ridgeRegression_udf.R
@@ -14,38 +14,43 @@ ridgeRegression = function(y, X, lambda)
 {
   beta_ridge = solve(t(X) %*% X + lambda * diag(1, dim(X)[2])) %*% t(X) %*% y
   ridgePredict = function (X){return (X %*% beta_ridge)}
-  result = list(lambda = lambda, coef_ridge = beta_ridge, ridgePredict = ridgePredict)
+  result = list(lambda = lambda, coef_ridge = beta_ridge, modelPredict = ridgePredict)
   return(result)
 }
-result=ridgeRegression(y, X, 0.01)
+
 
 
 #cross validation 
 #input: y, X, lambda, fold, criterion, randomseed; output: criterion value, figure, max point
 
 
-computeOneFold = function(reindex, y_ridge = y_ridge, X_ridge = X_ridge, lambda = lambda)
+computeOneFold = function(reindex, y_fun, X_fun, lambda = 0, fun)
 {
-  fitmodel = ridgeRegression(y = y_ridge[-reindex], X = X_ridge[-reindex,], lambda=lambda)
-  y_pre = fitmodel$ridgePredict(X = X_ridge[reindex,])
-  rss = sum((y_ridge[reindex] - y_pre)^2)
+  fitmodel = fun(y = y_fun[-reindex], X = X_fun[-reindex,], lambda=lambda)
+  y_pre = fitmodel$modelPredict(X = X_fun[reindex,])
+  rss = sum((y_fun[reindex] - y_pre)^2)
   return(rss)
 }
-
-ridgeCV = function(lambda, y_ridge, X_ridge, fold = fold, randomseed = randomseed)
+getFolddivide = function (samplesize,fold = 5, randomseed = NULL)
 {
   #divide into folds
   if(!is.null(randomseed)) set.seed(randomseed)
-  reindex=sample(1:length(y_ridge), size=length(y_ridge), replace = F)
-  foldsize = length(y_ridge) %/% fold
+  reindex=sample(1:samplesize, size=samplesize, replace = F)
+  foldsize = samplesize %/% fold
   folddivide = list()
   for (f in 1:fold)
   {
     folddivide = c(folddivide, list(reindex[((f-1)*foldsize+1):(f*foldsize)]))
   }
-  rss = sum(sapply(folddivide,FUN = computeOneFold, y_ridge=y_ridge, X_ridge = X_ridge, lambda=lambda))
-  N = length(y_ridge)
-  eigenvalue = eigen(t(X_ridge)%*%X_ridge)$values
+  return(folddivide)
+}
+
+getCV = function(lambda = 0, y_fun, X_fun, fun, fold = 5, folddivide = NULL, randomseed = NULL)
+{
+  if (is.null(folddivide)) folddivide = getFolddivide(length(y_fun), fold = fold, randomseed = randomseed)
+  rss = sum(sapply(folddivide,FUN = computeOneFold, y_fun=y_fun, X_fun = X_fun, lambda=lambda, fun=fun))
+  N = length(y_fun)
+  eigenvalue = eigen(t(X_fun)%*%X_fun)$values
   df =  sum(eigenvalue / (eigenvalue + lambda))
   mse = rss / N
   aic = N * log(rss / N) + 2 * df
@@ -53,10 +58,11 @@ ridgeCV = function(lambda, y_ridge, X_ridge, fold = fold, randomseed = randomsee
   return(c(lambda = lambda, MSE = mse, AIC = aic, BIC = bic))
 }
 
-ridgeLambdaChoosing = function(lambdaseq, y_ridge, X_ridge, fold = 5, randomseed = NULL)
+LambdaChoosing = function(lambdaseq, y_fun, X_fun, fun,fold = 5, randomseed = NULL)
 {
-  result = t(sapply(lambdaseq, FUN = ridgeCV,
-                    y_ridge = y_ridge, X_ridge = X_ridge, fold = fold, randomseed = randomseed))
+  folddivide = getFolddivide(length(y_fun),fold = fold, randomseed = randomseed)
+  result = t(sapply(lambdaseq, FUN = getCV,
+                    y_fun = y_fun, X_fun = X_fun, folddivide = folddivide, fun = fun))
   par(mfrow = c(3,1))
   plot(lambdaseq, result[,2], main = 'MSE', ylab = 'mse',type='l')
   plot(lambdaseq, result[,3], main = 'AIC', ylab = 'aic',type='l')
@@ -67,7 +73,6 @@ ridgeLambdaChoosing = function(lambdaseq, y_ridge, X_ridge, fold = 5, randomseed
   return(list(result=result, lambda_mse=lambda_mse, 
               lambda_aic=lambda_aic, lambda_bic=lambda_bic))
 }
-
 
 ##data simulation 
 set.seed(6646)
@@ -84,18 +89,6 @@ X=rmvnorm(n = N,mean = rep(0,p),sigma = sigma_mat)
 y=2*X[,1]+sqrt(6)*X[,2]+3*X[,3]-0.6*X[,4]+rnorm(n=N,mean=0,sd=1)
 
 
-ridgeLambdaChoosing(seq(0,1,0.005),y,X,fold=10)
-finalmodel = ridgeRegression(y, X, lambda=0.001)
-finalmodel$coef_ridge
-#second data
-N = 500; p = 4
-set.seed(6648)
-sigma_mat = matrix(0.2, ncol = p, nrow = p)
-for (i in 1:p){sigma_mat[i,i] = 1}
-X=rmvnorm(n = N, mean = rep(0, p), sigma = sigma_mat)
-X[,2] = 0.75 * X[,3] + 0.5 * X[,4] + rnorm(n = N, mean = 0, sd = 1)
-y = 1.5 * X[,1] +2 * X[,2] - 0.7 * X[,3] +0.9 * X[,4] + rnorm(n = N, mean = 0, sd = 1)
+result = LambdaChoosing(seq(0,1,0.02),y,X,fun=ridgeRegression,fold=10,randomseed = 6543)
+result[-1]
 
-ridgeLambdaChoosing(seq(0,2,0.01), y, X, fold = 10, randomseed = 6645)
-finalmodel = ridgeRegression(y, X, 1.13)
-finalmodel$coef_ridge
